cdd51fe756472447f5f2d3a0d4429c74a18e063c
Shashwat Singh <shashwat123student@gmail.com>
2023-09-27 19:41:46 +0530

    Added another page

---
+++ my-app/src/pages/Todos.tsx
@@ -0,0 +1,67 @@
+import {Button, Card, CardActions, CardContent} from "@mui/material";
+import {useEffect, useState} from "react";
+import styles from "../styles/Todos.module.css";
+function Todos(){
+    const [Todo, SetTodo] = useState([]);
+    const fetchTodos = ()=>{
+        fetch("http://localhost:3000/todos", {
+            method: "GET",
+        }).then((res) => {
+            res.json().then((data) => {
+                SetTodo(data.Todos);
+            });
+        });
+    }
+
+    useEffect(() => {
+        fetch("http://localhost:3000/todos", {
+            method: "GET",
+        }).then((res) => {
+            res.json().then((data) => {
+                SetTodo(data.Todos);
+            });
+        });
+    }, []);
+
+
+    return(
+        <div>
+            {Todo.map((data) => (
+                <Card key={data.id} sx={{
+                    bgcolor: "#454545",
+                    borderRadius: "0",
+                    borderBottom: "1px solid white",
+                }}>
+                    <CardContent>
+                        <div className={styles.titlecard}>
+                            {data.title}
+                        </div>
+                        <br/>
+                        <div className={styles.descriptioncard}>
+                            {data.description}
+                        </div>
+                    </CardContent>
+                    <CardActions>
+                        <Button sx={{
+                            color: "white",
+                        }}
+                                size="small"
+                                onClick={() => {
+                                    fetch(`http://localhost:3000/todos/${data.id}`, {
+                                        method: "DELETE",
+                                        headers: {
+                                            "Content-type": "application/json",
+                                        },
+                                    }).then(() => fetchTodos());
+                                }}
+                        >
+                            DELETE
+                        </Button>
+                    </CardActions>
+                </Card>
+            ))}
+        </div>
+    )}
+
+
+export default Todos;
