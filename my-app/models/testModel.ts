4a625ddf796f44c50beccfeb251d1e3f1d09b11c
Shashwat Singh <shashwat123student@gmail.com>
2023-09-28 17:57:36 +0530

    Trying to add mongodb

---
+++ my-app/models/testModel.ts
@@ -0,0 +1,11 @@
+import {Schema, model, models} from "mongoose";
+
+const todoSchema = new Schema({
+    id: Number,
+    title: String,
+    description: String,
+});
+
+const Todo = models.Todo ||  model("Todo", todoSchema);
+
+export default Todo;
