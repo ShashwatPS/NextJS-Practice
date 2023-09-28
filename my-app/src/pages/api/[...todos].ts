2dbd0eccc6d8e783beedd8336e797e31fd8b4a88
Shashwat Singh <shashwat123student@gmail.com>
2023-09-28 22:18:35 +0530

    Still trying to fix backend

--- my-app/src/pages/api/[...todos].ts
+++ my-app/src/pages/api/[...todos].ts
@@ -1,46 +1,40 @@
-import connectMongo from '../../../utils/db.js';
-import { NextApiRequest, NextApiResponse } from 'next';
-import Todo from '../../../models/testModel';
+import {NextApiRequest, NextApiResponse} from "next";
+import connectMongo from "../../../utils/db.js";
+import Todo from "../../../models/testModel";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler( req: NextApiRequest, res: NextApiResponse){
     await connectMongo();
-
-    const id = Number(req.query.id);
-
-    if (req.method === 'GET') {
-        try {
-            const todo = await Todo.findById(id);
-            if (!todo) {
-                res.status(404).json({ message: "Todo Not Found" });
-            } else {
-                res.status(200).json({ todo });
-            }
-        } catch (error) {
-            res.status(500).json({ error: "Error fetching todo" });
+    const { id } = req.query;
+    if(req.method === 'GET'){
+        const todo = await Todo.findById(id);
+        if (!todo) {
+            res.status(404);
+        } else {
+            res.json({ todo });
         }
-    } else if (req.method === 'PUT') {
-        try {
-            const todo = await Todo.findByIdAndUpdate(id, req.body, {
-                new: true,
-            });
-            if (!todo) {
-                res.status(404).json({ message: "Todo Not Found" });
-            } else {
-                res.status(200).json({ message: "Todo Updated Successfully" });
-            }
-        } catch (error) {
-            res.status(500).json({ error: "Error updating todo" });
+    }
+    else if(req.method === 'PUT'){
+        const todo = await Todo.findByIdAndUpdate(id, req.body, {
+            new: true,
+        });
+        if (!todo) {
+            res.status(404).json({message: "Todo Not Found"});
+        } else {
+            res.status(200).json({ message: "Todo Updated Successfully" });
         }
-    } else if (req.method === 'DELETE') {
+    }
+    else if(req.method === 'DELETE'){
         try {
             const deletedTodo = await Todo.deleteOne({ id: id });
             if (deletedTodo.deletedCount === 0) {
-                res.status(404).json({ message: "Todo not found" });
+                res.status(404).json({message: "Todo not found"});
             } else {
-                res.status(200).json({ message: "Todo Deleted successfully" });
+                res.status(200).json({message: "Todo Deleted successfully"});
             }
         } catch (error) {
-            res.status(500).json({ error: "Error deleting todo" });
+            res
+                .status(500)
+                .send({ error: "Error deleting todo"});
         }
     }
 }
